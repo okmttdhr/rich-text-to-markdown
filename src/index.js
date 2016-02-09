8aa06ed29139458a78c105234096073e73ce37f8
okmttdhr <10101114fin@gmail.com>
2016-02-09 13:12:14 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import { React, Route, Application,
-         connect, bindActionCreators } from 'reactuate'
+import 'medium-editor/dist/css/medium-editor.css'
+import 'medium-editor/dist/css/themes/default.css'
+import {
+  React,
+  Route,
+  Application,
+  connect,
+  bindActionCreators
+} from 'reactuate'
+import MediumEditor from 'medium-editor'
 
 import counter from './counter'
 import counterAsync from './counter/async'
@@ -11,50 +19,17 @@ class App extends React.Component {
 }
 
 class HomePage extends React.Component {
-  handleIncrement() {
-    this.props.actions.IncrementCounter()
+  componentDidMount() {
+    const elements = document.querySelectorAll('.editable')
+    const editor = new MediumEditor(elements)
   }
-  handleIncrementDelayed() {
-    this.props.actions.IncrementCounterDelayed()
-  }
+  
   render() {
-    return (<div>
-     <h1>Reactuate Application</h1>
-     <p>
-     Congratulations! You are running a Reactuate application now. Here is what you need to do to start developing your own application:
-     </p>
-     <ol>
-       <li>Unless you have done so already, add a start script to your package.json:
-        <pre><code>
-{`"scripts": {
-  "start": "node node_modules/reactuate/webpack-dev-server.js"
-}`}
-        </code></pre>
-        This way you can easily run your application:
-        <pre><code>
-{`$ npm start`}
-        </code></pre>
-       </li>
-       <li>Also, add this to your package.json
-       <pre><code>
-{`"scripts": {
-  "build": "NODE_ENV=production webpack --config node_modules/reactuate/default-webpack-config.js --progress --colors"
-}`}
-       </code></pre>
-       This way you can easily make a production build of your application:
-       <pre><code>
-{`$ npm run build`}
-       </code></pre>
-       </li>
-       <li>Copy the starter file from {`${typeof REACTUATE_DIRNAME === 'undefined' ? "<reactuate package dir>" : REACTUATE_DIRNAME}/sample/index.js`} to src/index.js</li>
-     </ol>
-     <div>
-       <h5>Counter example</h5>
-       {this.props.counter}
-       <button onClick={() => this.handleIncrement()}>Increment</button>
-       <button onClick={() => this.handleIncrementDelayed()}>Increment with delay</button>
-     </div>
-    </div>)
+    return (
+      <div>
+        <div className='editable'>test test</div>
+      </div>
+    )
   }
 }
 
